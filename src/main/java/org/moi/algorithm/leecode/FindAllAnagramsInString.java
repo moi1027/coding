3f26271339489a672d3ea79054fa0bc7e9450c94
moi <912546506@qq.com>
2020-11-19 21:46:50 +0800

    力扣的第20题，快手笔试题 用栈解决

---
+++ src/main/java/org/moi/algorithm/leecode/FindAllAnagramsInString.java
@@ -0,0 +1,73 @@
+package org.moi.algorithm.leecode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program:
+ * @description: leeCode 第438题 找到字符串中所有字母异位词 难度：中级
+ * @author: moi
+ * @create: 2020/11/19 20:49
+ **/
+public class FindAllAnagramsInString {
+
+    /**
+     * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+     *
+     * 字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+     *
+     */
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        String[] a = s.split("");
+        String[] b = p.split("");
+        Map<String,Integer> baseMap = new HashMap<String, Integer>();
+        Map<String,Integer> targetMap = new HashMap<String, Integer>();
+        for(String c:b){
+            if(baseMap.containsKey(c)){
+                baseMap.put(c,baseMap.get(c)+1);
+            }else{
+                baseMap.put(c,1);
+            }
+        }
+        int left = 0,right = 0;
+        int variable = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        while(right<a.length){
+            String d = a[right];
+            right++;
+            if(baseMap.containsKey(d)){
+                if(targetMap.containsKey(d)){
+                    targetMap.put(d,targetMap.get(d)+1);
+                }else{
+                    targetMap.put(d,1);
+                }
+                if(targetMap.get(d).equals(baseMap.get(d))){
+                    variable++;
+                }
+            }
+            while(variable == baseMap.size()){
+                if(right-left == b.length){
+                    list.add(left);
+                }
+                String e = a[left];
+                left++;
+                if(baseMap.containsKey(e)){
+                    if(targetMap.get(e).equals(baseMap.get(e))){
+                        variable--;
+                    }
+                    targetMap.put(e,targetMap.get(e)-1);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String s = "abab";
+        String p = "ab";
+        findAnagrams(s,p);
+    }
+}
---
+++ src/main/java/org/moi/algorithm/leecode/ValidParentheses.java
@@ -0,0 +1,59 @@
+package org.moi.algorithm.leecode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * @program:
+ * @description: leeCode 第20题 有效的括号 快手出现过此题！！！ 难度 ：easy
+ * @author: moi
+ * @create: 2020/11/19 21:34
+ **/
+public class ValidParentheses {
+
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 注意空字符串可被认为是有效字符串。
+     *
+     */
+
+    public static boolean isValid(String s) {
+        int len = s.length();
+        //如果字符串不是偶数的长度则必然闭合不了
+        if(len % 2 != 0){
+            return false;
+        }
+        Map<Character,Character> map =new HashMap<Character, Character>(){{
+            put(')','(');
+            put(']','[');
+            put('}','{');
+        }};
+        //采用栈的解决方式 对比成功之后直接出栈 要保证括号的顺序
+        Deque<Character> stack = new LinkedList<Character>();
+        for(int i = 0;i<len;i++){
+            Character a = s.charAt(i);
+            if(map.containsKey(a)){
+                if(stack.isEmpty() || !stack.peek().equals(map.get(a))){
+                    return false;
+                }
+                stack.pop();
+            }else{
+                stack.push(a);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+}
