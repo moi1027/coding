31519622296c6686d458df85b65a2833e189aa6f
moi <912546506@qq.com>
2020-12-09 22:14:58 +0800

    leecode 排序

---
+++ src/main/java/org/moi/algorithm/leecode/sort/MinKNumber.java
@@ -0,0 +1,13 @@
+package org.moi.algorithm.leecode.sort;
+
+/**
+ * @program:
+ * @description: 面试题17 最小K个数
+ * @author: moi
+ * @create: 2020/12/9 22:14
+ **/
+public class MinKNumber {
+    /**
+     * 题目：设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+     */
+}
