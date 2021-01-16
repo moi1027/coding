f8cb24d6e02bd0ee8a75d3300b40c53fdd462be1
moi <912546506@qq.com>
2021-01-16 23:41:52 +0800

    leecode 每日一题

---
+++ src/main/java/org/moi/algorithm/simple/classic/NQueen.java
@@ -0,0 +1,66 @@
+package org.moi.algorithm.simple.classic;
+
+/**
+ * @program:
+ * @description: N皇后问题
+ * @author: moi
+ * @create: 2021/1/16 23:05
+ **/
+public class NQueen {
+
+    /**
+     * N皇后定义：在一个N*N的棋盘上，有N个皇后，每两个皇后都不同行不同列、不同斜线
+     *          求有多少种摆法
+     */
+
+
+    public static int num1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        // record[0] ?  record[1]  ?  record[2]
+        int[] record = new int[n]; // record[i] -> i行的皇后，放在了第几列
+        return process1(0, record, n);
+    }
+
+    // 潜台词：record[0..i-1]的皇后，任何两个皇后一定都不共行、不共列，不共斜线
+    // 目前来到了第i行
+    // record[0..i-1]表示之前的行，放了的皇后位置
+    // n代表整体一共有多少行  0~n-1行
+    // 返回值是，摆完所有的皇后，合理的摆法有多少种
+    public static int process1(int i, int[] record, int n) {
+        if (i == n) { // 终止行
+            return 1;
+        }
+        // 没有到终止位置，还有皇后要摆
+        int res = 0;
+        for (int j = 0; j < n; j++) { // 当前行在i行，尝试i行所有的列  -> j
+            // 当前i行的皇后，放在j列，会不会和之前(0..i-1)的皇后，不共行共列或者共斜线，
+            // 如果是，认为有效
+            // 如果不是，认为无效
+            if (isValid(record, i, j)) {
+                record[i] = j;
+                res += process1(i + 1, record, n);
+            }
+        }
+        return res;
+    }
+
+    // record[0..i-1]你需要看，record[i...]不需要看
+    // 返回i行皇后，放在了j列，是否有效
+    public static boolean isValid(int[] record, int i, int j) {
+        for (int k = 0; k < i; k++) { // 之前的某个k行的皇后
+            // k, record[k]   i, j
+            if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(num1(n));
+    }
+
+}
